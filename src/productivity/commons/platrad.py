4809c2dfe1e62e936b9ef8a914424c59bb2aa058
Mohan Das <mohandaskj.11@gmail.com>
2026-01-15 18:18:27 +0530

    Fix return statements in OS detection functions in platrad.py

--- src/productivity/commons/platrad.py
+++ src/productivity/commons/platrad.py
@@ -5,11 +5,14 @@ import os
 def isWindows():
     if platform.system == "Windows":
         return True
+    return False
     
 def isLinux():
     if platform.system == "Linux":
         return True
+    return False
 
 def isTermux():
     if "ANDROID_ROOT" in os.environ or "com.termux" in os.environ.get("TERM",""):
         return True
+    return False
